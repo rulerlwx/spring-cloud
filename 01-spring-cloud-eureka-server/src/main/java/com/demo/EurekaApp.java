4b96d63397c36380b6181ac99ef38f21937d96c2
black_wolf <leafsing20309@163.com>
2019-06-22 00:26:17 +0800

    eureka集群——windows环境

---
+++ 01-spring-cloud-eureka-server/src/main/java/com/demo/EurekaApp.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by lwx on 2019/6/19.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApp.class, args);
+    }
+}
